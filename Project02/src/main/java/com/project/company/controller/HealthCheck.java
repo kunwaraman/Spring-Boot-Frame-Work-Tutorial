1af1c24b6d574c1f2c09b51a83d1c2282fc07ee0
kunwaraman <amankunwar8472@gmail.com>
2024-11-04 22:04:12 +0530

    journal

---
+++ Project02/src/main/java/com/project/company/controller/HealthCheck.java
@@ -0,0 +1,16 @@
+package com.project.company.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheck {
+	
+	@GetMapping("/health-check")
+	public String healthCheck() {
+		return "ok";
+		
+	}
+	
+
+}
---
+++ Project02/src/main/java/com/project/company/controller/JournalEntryController.java
@@ -0,0 +1,33 @@
+package com.project.company.controller;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.company.entity.JournalEntry;
+
+@RestController
+@RequestMapping("/journal")
+public class JournalEntryController {
+	
+	private HashMap<Long,JournalEntry> journalEntries = new HashMap<>();
+	
+	@GetMapping
+	public List<JournalEntry> getAll(){
+		return new ArrayList<>(journalEntries.values());
+	}
+	
+	@PostMapping
+	public boolean createEntry(@RequestBody JournalEntry myEntry) {
+		journalEntries.put(myEntry.getId(), myEntry);
+		return true;
+		
+	}
+	
+}
